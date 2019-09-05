a79c3eb5878f90c7bc41a486dbfa45cbba9e50ef
Paul Revelo <prevelo01@manhattan.edu>
2019-09-05 17:40:04 -0400

    Added the plot

--- Project-01-Simple-Application-of-Numerical-Comp/model.m
+++ Project-01-Simple-Application-of-Numerical-Comp/model.m
@@ -1,6 +1,7 @@
-% Statement: 
-%
+% Statement: Plot the function L vs c
+% X -  Vector
 
 syms L c
-L = @(c) 9/sin(pi - 2.1468 - c) + 7/sin(c);
-x = zeros(1: 5: 100);
+L = @(c) 9./sin(pi - 2.1468 - c) + 7./sin(c);
+x = linspace(-100, 100, 1000);
+plot(x, L(x));
